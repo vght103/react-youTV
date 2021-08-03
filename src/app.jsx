23d49c3abe349ea73f007cbfd1716c990f4e129d
vght103 <74849404+vght103@users.noreply.github.com>
2021-08-03 15:28:58 +0900

    add login components

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,14 @@
-import "./app.css";
+import styles from "./app.module.css";
+import Login from "./components/login/login";
 
-function App() {
-  return <h1>hello</h1>;
+function App({ authService }) {
+  return (
+    <>
+      <div className={styles.app}>
+        <Login authService={authService} />
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/login/login.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Footer from "../footer/footer";
+import Header from "../header/header";
+import styles from "./login.module.css";
+
+const Login = ({ authService }) => {
+  // 로그인 할 수 있는 함수
+  console.log(authService);
+  const onLogin = (event) => {
+    authService //
+      .login(event.currentTarget.textContent)
+      .then(console.log);
+  };
+
+  return (
+    <section className={styles.login}>
+      <Header />
+      <section>
+        <h1>Login</h1>
+        <ul className={styles.list}>
+          <li className={styles.item}>
+            <button className={styles.button} onClick={onLogin}>
+              Google
+            </button>
+          </li>
+          <li className={styles.item}>
+            <button className={styles.button} onClick={onLogin}>
+              Github
+            </button>
+          </li>
+        </ul>
+      </section>
+      <Footer />
+    </section>
+  );
+};
+
+export default Login;
