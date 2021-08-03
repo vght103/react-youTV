a72c3bc50bda4223d1d93127f183398fca1c1ad0
vght103 <74849404+vght103@users.noreply.github.com>
2021-08-03 12:05:00 +0900

    firebase login update

---
+++ src/service/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+};
+console.log(firebaseConfig.apiKey);
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
