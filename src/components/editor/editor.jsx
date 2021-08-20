8d4abec104c3489cc50d92709713211b0a1e65d6
vght103 <74849404+vght103@users.noreply.github.com>
2021-08-20 18:17:31 +0900

    business card reupload

--- src/components/editor/editor.jsx
+++ src/components/editor/editor.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import styles from "./editor.module.css";
-import CardEditFrom from "../card_edit_form/card_edit_from";
+import CardEditForm from "../card_edit_form/card_edit_form";
 import CardAddForm from "../card_add_form/card_add_form";
 
 const Editor = ({ cards, onAddCard }) => (
   <section className={styles.editor}>
     <h2 className={styles.title}>Card Maker</h2>
     {cards.map((card) => (
-      <CardEditFrom key={card.id} card={card} />
+      <CardEditForm key={card.id} card={card} />
     ))}
     <CardAddForm onAddCard={onAddCard} />
   </section>
--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,5 +1,8 @@
 import firebase from "firebase";
 
+// import * as firebase from "firebase/app";
+import "firebase/database";
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
